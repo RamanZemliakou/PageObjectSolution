16b7fdb5197acf9158877a2fbc98104d8f75c3ab
Raman Zemliakou <raman.zemliakou@gmail.com>
2022-05-14 19:26:36 +0300

    added PageObjects and Test

---
+++ PageOblectSolution1/UnitTest1.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using PageOblectSolution1.PaegObjects;
+
+namespace PageOblectSolution1
+{
+    public class Tests
+    {
+        IWebDriver _webDriver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _webDriver = new ChromeDriver();
+            _webDriver.Manage().Window.Maximize();
+            _webDriver.Navigate().GoToUrl("https://demoqa.com/text-box");
+        }
+
+        [Test]
+        public void OutputFieldTest()
+        {
+            var textBoxPage = new TextBoxPageObject(_webDriver);
+            textBoxPage.InsertInputData();
+            string userName = textBoxPage.ReadData();
+                
+            var inputName = userName;
+            Assert.AreEqual("Name:" + userName, inputName);
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            _webDriver.Quit();
+        }
+    }
+}
